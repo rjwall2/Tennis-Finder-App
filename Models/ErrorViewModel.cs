69d39aff3cae1b1601f9d6254df228045c7048e9
Ryan Wall <rjwall2@students.cs.ubc.ca>
2024-09-06 17:42:44 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tennis_Finder_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
